615c11f0dcd6d38d17bf8335fb6d6feeb07656f7
Siftben <yobel.oktavinus98@gmail.com>
2023-09-04 01:35:50 +0900

    Add switch to pencil and eraser button

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 createSketch();
 const drawDivs = document.querySelectorAll('.draw-px');
 const colorPicker = document.getElementById("color-picker");
+const drawButton = document.querySelector('.draw-btn');
+const eraseButton = document.querySelector('.erase-btn');
 const resetButton = document.querySelector('.reset-btn');
-let colorDraw;
+let colorDraw = "black";
+let isPencil = true;
 
 function createSketch () {
     const drawbg = document.querySelector('.draw-bg');
@@ -39,8 +42,14 @@ function handleLeave() {
 }
 
 function draw() {
-    this.style.backgroundColor = colorDraw;
-    this.classList.add('drawn');
+    if(isPencil === true) {
+        this.style.backgroundColor = colorDraw;
+        this.classList.add('drawn');
+
+    } else {
+        this.style.backgroundColor = 'white';
+        this.classList.remove('drawn');
+    } 
 }
 
 function changeColor() {
@@ -50,18 +59,31 @@ function changeColor() {
 
 function reset() {
     drawDivs.forEach(divs => {
+
         if(divs.classList.contains('drawn')){
             divs.classList.remove('drawn');
             divs.style.backgroundColor = "white";
         }
-        
     });
-    
+}
+
+function handleDrawButton() {
+    eraseButton.style.backgroundColor = "white";
+    drawButton.style.backgroundColor = "grey";
+    isPencil = true;
+}
+
+function handleEraseButton(){
+    eraseButton.style.backgroundColor = "grey";
+    drawButton.style.backgroundColor = "white";
+    isPencil = false;
 }
 
 drawDivs.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
 drawDivs.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
 drawDivs.forEach(trigger => trigger.addEventListener('click', draw))
 
+drawButton.addEventListener('click', handleDrawButton);
+eraseButton.addEventListener('click', handleEraseButton);
 resetButton.addEventListener("click", reset);
 colorPicker.addEventListener("input", changeColor);
